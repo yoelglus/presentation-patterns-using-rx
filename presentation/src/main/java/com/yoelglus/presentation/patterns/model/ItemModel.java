26c26becbe95ab54e8ae22abb3d740a3eac608b0
memoizr <memoizrlabs@gmail.com>
2016-10-16 15:34:38 +0100

    add ItemDetailViewModelTest and ItemsListViewModelTest

--- presentation/src/main/java/com/yoelglus/presentation/patterns/model/ItemModel.java
+++ presentation/src/main/java/com/yoelglus/presentation/patterns/model/ItemModel.java
@@ -28,4 +28,33 @@ public class ItemModel {
     public static ItemModel from(Item item) {
         return new ItemModel(item.getId(), item.getContent(), item.getDetail());
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
+        ItemModel itemModel = (ItemModel) o;
+
+        if (mId != null ? !mId.equals(itemModel.mId) : itemModel.mId != null) {
+            return false;
+        }
+        if (mContent != null ? !mContent.equals(itemModel.mContent) : itemModel.mContent != null) {
+            return false;
+        }
+        return mDetail != null ? mDetail.equals(itemModel.mDetail) : itemModel.mDetail == null;
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = mId != null ? mId.hashCode() : 0;
+        result = 31 * result + (mContent != null ? mContent.hashCode() : 0);
+        result = 31 * result + (mDetail != null ? mDetail.hashCode() : 0);
+        return result;
+    }
 }
---
+++ presentation/src/test/java/com/yoelglus/presentation/patterns/mvvm/ItemDetailViewModelTest.java
@@ -0,0 +1,51 @@
+package com.yoelglus.presentation.patterns.mvvm;
+
+import com.yoelglus.presentation.patterns.data.ItemsRepository;
+import com.yoelglus.presentation.patterns.entities.Item;
+import com.yoelglus.presentation.patterns.model.ItemModel;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import rx.Scheduler;
+import rx.observers.TestSubscriber;
+import rx.schedulers.Schedulers;
+
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
+import static rx.Observable.just;
+
+@RunWith(MockitoJUnitRunner.class)
+public class ItemDetailViewModelTest {
+
+    private static final String CONTENT_TEXT = "Content";
+    private static final String DETAIL_TEXT = "detail";
+    private static final String ITEM_ID = "123";
+
+    private ItemsRepository itemsRepository = Mockito.mock(ItemsRepository.class);
+
+    private Scheduler ioScheduler = Schedulers.immediate();
+    private Scheduler mainScheduler = Schedulers.immediate();
+    private ItemDetailViewModel itemDetailViewModel =
+            new ItemDetailViewModel(itemsRepository, ITEM_ID, ioScheduler, mainScheduler);
+
+    @Before
+    public void setUp() {
+        when(itemsRepository.getItem(anyString()))
+                .thenReturn(just(new Item(ITEM_ID, CONTENT_TEXT, DETAIL_TEXT)));
+    }
+
+    @Test
+    public void should_emit_item_model_from_repository() {
+        TestSubscriber<ItemModel> testSubscriber = TestSubscriber.create();
+        itemDetailViewModel.itemModel().subscribe(testSubscriber);
+
+        itemDetailViewModel.onStart();
+
+        testSubscriber.assertValue(ItemModel.from(new Item(ITEM_ID, CONTENT_TEXT, DETAIL_TEXT)));
+    }
+}
+
